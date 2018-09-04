4174c7469d2695fc930aa3cc84bf8a123b88c9a6
ian.housman <ian.housman@gmail.com>
2018-09-04 03:30:15 +0000

    Updated gde-pulse/wrappers/getLandsatWrapper.js (added 1 line, deleted 1 line).

--- gde-pulse/wrappers/getLandsatWrapper.js
+++ gde-pulse/wrappers/getLandsatWrapper.js
@@ -123,7 +123,7 @@ var correctScale = 250;//Choose a scale to reduce on- 250 generally works well
 var exportComposites = true;
 
 //Set up Names for the export
-var outputName = 'Medoid-Landsat_';
+var outputName = 'Landsat_';
 
 //Provide location composites will be exported to
 //This should be an asset folder, or more ideally, an asset imageCollection
