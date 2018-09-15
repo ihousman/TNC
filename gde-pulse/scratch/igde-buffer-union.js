819cf9488f38120a093b3caffdd502e2cfbdbbda
ian.housman <ian.housman@gmail.com>
2018-09-15 04:03:39 +0000

    Updated gde-pulse/scratch/igde-buffer-union.js (added 9 lines, deleted 2 lines).

--- gde-pulse/scratch/igde-buffer-union.js
+++ gde-pulse/scratch/igde-buffer-union.js
@@ -1,5 +1,12 @@
 var sa = ee.FeatureCollection('projects/igde-work/igde-data/GDEpulse2018_iGDE_V1_20180802_joined_annual_depth_macro_veg')
-
+var studyArea = ee.Feature(ee.FeatureCollection('TIGER/2016/States')
+            .filter(ee.Filter.eq('NAME','California'))
+            .first())
+            .convexHull(10000)
+            .buffer(10000)
+            .geometry();
+Map.addLayer(studyArea)
 sa = sa.map(function(f){return f.buffer(20000,1000)}).union(1000)
-
+print(sa.size())
+Map.addLayer(sa)
 Export.table.toAsset(sa, 'Export','projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping')
