8c185d5993a5d05d0cbf5c220da2f8e38ee609c8
ian.housman <ian.housman@gmail.com>
2018-09-15 14:38:48 +0000

    Updated gde-pulse/wrappers/movingWindowZandTrendWrapper.js (added 1 line, deleted 1 line).

--- gde-pulse/wrappers/movingWindowZandTrendWrapper.js
+++ gde-pulse/wrappers/movingWindowZandTrendWrapper.js
@@ -26,7 +26,7 @@ var studyArea =sa.bounds();
 // startJulian: Starting Julian date 
 // endJulian: Ending Julian date
 var startJulian = 121;
-var endJulian = 121+64
+var endJulian = 121+(64*2)
 
 // 3. Specify start and end years for all analyses
 // More than a 3 year span should be provided for time series methods to work 
