4a6a0ebe4479f1ac9d0d26b6f7e315e80ef03047
ian.housman <ian.housman@gmail.com>
2018-09-15 04:15:19 +0000

    Updated gde-pulse/scratch/igde-buffer-union.js (added 2 lines).

--- gde-pulse/scratch/igde-buffer-union.js
+++ gde-pulse/scratch/igde-buffer-union.js
@@ -9,4 +9,6 @@ Map.addLayer(studyArea)
 sa = sa.map(function(f){return f.buffer(20000,1000)}).union(1000)
 print(sa.size())
 Map.addLayer(sa)
+var output = ee.FeatureCollection('projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping');
+Map.addLayer(output)
 Export.table.toAsset(sa, 'Export','projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping')
