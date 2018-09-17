443956fe812fca797edbb843fb7f1868132e2ed9
ian.housman <ian.housman@gmail.com>
2018-09-17 21:01:48 +0000

    Updated gde-pulse/wrappers/EWMACDWrapper.js (added 13 lines, deleted 9 lines).

--- gde-pulse/wrappers/EWMACDWrapper.js
+++ gde-pulse/wrappers/EWMACDWrapper.js
@@ -220,15 +220,19 @@ indexNames.map(function(indexName){
 });
 
 //Export each years EWMACD output
-var years = ee.List.sequence(startYear,endYear).getInfo();
+// var years = ee.List.sequence(startYear,endYear).getInfo();
 
-  years.map(function(year){
-    var ewmaYr = ee.Image(outputCollection.filter(ee.Filter.calendarRange(year,year,'year')).first())
-    .int16();
+//   years.map(function(year){
+//     var ewmaYr = ee.Image(outputCollection.filter(ee.Filter.calendarRange(year,year,'year')).first())
+//     .int16();
     
-  var exportName = outputName+'_' + year.toString();
-    var exportPath = exportPathRoot + '/'+exportName;
+//   var exportName = outputName+'_' + year.toString();
+//     var exportPath = exportPathRoot + '/'+exportName;
     
-    getImageLib.exportToAssetWrapper(ewmaYr,exportName,exportPath,'mean',
-      studyArea,null,crs,transform);
-  });
+//     getImageLib.exportToAssetWrapper(ewmaYr,exportName,exportPath,'mean',
+//       studyArea,null,crs,transform);
+//   });
+
+
+var ewmaOut = ee.ImageCollection('users/ianhousman/test/changeCollection');
+Map.addLayer(ewmaOut)
