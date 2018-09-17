7e7c9784c422577c0db8da1bd9e50feb758694a0
ian.housman <ian.housman@gmail.com>
2018-09-17 03:22:08 +0000

    Updated gde-pulse/viewers/predictorViewer.js (added 1 line, deleted 1 line).

--- gde-pulse/viewers/predictorViewer.js
+++ gde-pulse/viewers/predictorViewer.js
@@ -13,7 +13,7 @@ var sa = ee.FeatureCollection('projects/igde-work/igde-data/igde_buffer_20k_unio
 var igdes = ee.FeatureCollection('projects/igde-work/igde-data/GDEpulse2018_iGDE_V1_20180802_joined_annual_depth_macro_veg');
 
 var lt = ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection')
-        .map(function(img){return dLib.multBands(img,1,0.001))});
+        .map(function(img){return dLib.multBands(img,1,0.001)});
 var harmonics = ee.ImageCollection('projects/igde-work/raster-data/harmonic-coefficients-collection');
 harmonics = harmonics.map(function(img){
   var yr = ee.Number.parse(img.id().split('_').get(2)).add(1);
