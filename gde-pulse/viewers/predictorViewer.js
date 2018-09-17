1383c19901735fcd28a9d772e74b7f47154081f7
ian.housman <ian.housman@gmail.com>
2018-09-17 03:21:39 +0000

    Updated gde-pulse/viewers/predictorViewer.js (added 3 lines, deleted 2 lines).

--- gde-pulse/viewers/predictorViewer.js
+++ gde-pulse/viewers/predictorViewer.js
@@ -12,11 +12,12 @@ dLib.getExistingChangeData();
 var sa = ee.FeatureCollection('projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping').geometry();
 var igdes = ee.FeatureCollection('projects/igde-work/igde-data/GDEpulse2018_iGDE_V1_20180802_joined_annual_depth_macro_veg');
 
-var lt = ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection');
+var lt = ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection')
+        .map(function(img){return dLib.multBands(img,1,0.001))});
 var harmonics = ee.ImageCollection('projects/igde-work/raster-data/harmonic-coefficients-collection');
 harmonics = harmonics.map(function(img){
   var yr = ee.Number.parse(img.id().split('_').get(2)).add(1);
-  return ee.Image(dLib.multBands(img,1,0.001)).float().set({'system:time_start':ee.Date.fromYMD(yr,6,1).millis(),
+  return ee.Image(dLib.multBands(img,1,0.001)).set({'system:time_start':ee.Date.fromYMD(yr,6,1).millis(),
                   'modelLength':3,'noDependents':9})
 })
 var zTrend =ee.ImageCollection('projects/igde-work/raster-data/z-score-trend-collection');
