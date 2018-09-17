590766f83b3a64bbf5486a7c0cd471c07522976e
ian.housman <ian.housman@gmail.com>
2018-09-17 02:29:57 +0000

    Updated gde-pulse/viewers/predictorViewer.js (added 1 line, deleted 2 lines).

--- gde-pulse/viewers/predictorViewer.js
+++ gde-pulse/viewers/predictorViewer.js
@@ -16,7 +16,7 @@ var lt = ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collectio
 var harmonics = ee.ImageCollection('projects/igde-work/raster-data/harmonic-coefficients-collection');
 harmonics = harmonics.map(function(img){
   var yr = ee.Number.parse(img.id().split('_').get(2)).add(1);
-  return img.set({'system:time_start':ee.Date.fromYMD(yr,6,1).millis(),
+  return dLib.multBands(img,1,0.001).set({'system:time_start':ee.Date.fromYMD(yr,6,1).millis(),
                   'modelLength':3,'noDependents':9})
 })
 var zTrend =ee.ImageCollection('projects/igde-work/raster-data/z-score-trend-collection');
@@ -24,7 +24,6 @@ var zTrend =ee.ImageCollection('projects/igde-work/raster-data/z-score-trend-col
 
 
 var pap = harmonics
-    .map(function(img){return dLib.multBands(img,1,0.001)})
     .map(getImageLib.getPhaseAmplitudePeak);
 
 var amplitudes = pap.select(['.*_amplitude']);
