fd4186a8dfae25589bafe6b5c98af9bfeaa40592
ian.housman <ian.housman@gmail.com>
2018-10-09 03:38:02 +0000

    Updated gde-pulse/scratch/igde-buffer-union.js (added 6 lines, deleted 5 lines).

--- gde-pulse/scratch/igde-buffer-union.js
+++ gde-pulse/scratch/igde-buffer-union.js
@@ -1,6 +1,7 @@
 var sa = ee.FeatureCollection('projects/igde-work/igde-data/GDEpulse2018_iGDE_V1_20180802_joined_annual_depth_macro_veg')
 var sa2 =  ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018');
-
+// Map.addLayer(sa,{color:'red'},'original')
+Map.addLayer(sa2,{color:'blue'},'new')
 var studyArea = ee.Feature(ee.FeatureCollection('TIGER/2016/States')
             .filter(ee.Filter.eq('NAME','California'))
             .first())
@@ -11,8 +12,8 @@ Map.addLayer(studyArea,{},'convexHull',false)
 sa = sa.map(function(f){return f.buffer(20000,1000)}).union(1000);
 sa2 = sa2.map(function(f){return f.buffer(20000,1000)}).union(1000);
 print(sa.size())
-Map.addLayer(sa)
-Map.addLayer(sa2)
-// var output = ee.FeatureCollection('projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping');
-// Map.addLayer(output)
+// Map.addLayer(sa,{color:'red'},'original')
+// Map.addLayer(sa2,{color:'blue'},'new')
+var output = ee.FeatureCollection('projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping');
+Map.addLayer(output)
 Export.table.toAsset(sa, 'Export','projects/igde-work/igde-data/igde_buffer_20k_union_for_clipping')
