41cb5cc1d371c8cc69c484327d0c1e324f96f614
ian.housman <ian.housman@gmail.com>
2018-10-09 03:40:31 +0000

    Updated gde-pulse/viewers/predictorViewer.js (added 1 line, deleted 1 line).

--- gde-pulse/viewers/predictorViewer.js
+++ gde-pulse/viewers/predictorViewer.js
@@ -132,7 +132,7 @@ joinedRaw = addPrefixToCollectionBandNames(joinedRaw,'D0_')
 zTrend = addPrefixToCollectionBandNames(zTrend,'D1_')
 
 // igdes = igdes.limit(50);
-var out = ee.List.sequence(1991,1992).getInfo().map(function(yr){
+var out = ee.List.sequence(1985,2018).getInfo().map(function(yr){
   var yro = yr;
   yr = ee.Number(yr);
   var rawPre = ee.Image(joinedRawForSlope.filter(ee.Filter.calendarRange(yr.subtract(1),yr.subtract(1),'year')).first());
