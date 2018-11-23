99da45f8bafb7a9c0dfed7f24d563a3ca0d2468d
ian.housman <ian.housman@gmail.com>
2018-11-23 13:51:43 +0000

    Updated gde-pulse/viewers/predictorViewer.js (added 3 lines, deleted 3 lines).

--- gde-pulse/viewers/predictorViewer.js
+++ gde-pulse/viewers/predictorViewer.js
@@ -135,7 +135,7 @@ function getPairDiff(c,year){
   
   var cSlpT  =cT2.subtract(cT1).float();
   print('cslp',cT1,year)
-  return [cSlpT];//[addPrefixToImageBandNames(cSlpT,'D0_'),addPrefixToImageBandNames(cSlpT,'D1_')];
+  return [addPrefixToImageBandNames(cT2,'D0_'),addPrefixToImageBandNames(cSlpT,'D1_')];
 }
 
 function getYr(year){
@@ -145,8 +145,8 @@ function getYr(year){
   var papPair = getPairDiff(pap,year);
   var daymetPair = getPairDiff(daymet,year);
   var zPair = getPairDiff(zTrend,year);
-  var out =ee.Image(igdeyrPair[0])//.addBands(compPair[0]).addBands(ltPair[0]).addBands(papPair[0]).addBands(daymetPair[0])
-           // .addBands(igdeyrPair[1]).addBands(compPair[1]).addBands(ltPair[1]).addBands(papPair[1]).addBands(daymetPair[1]).addBands(zPair[1])
+  var out =ee.Image(igdeyrPair[0]).addBands(compPair[0]).addBands(ltPair[0]).addBands(papPair[0]).addBands(daymetPair[0])
+            .addBands(igdeyrPair[1]).addBands(compPair[1]).addBands(ltPair[1]).addBands(papPair[1]).addBands(daymetPair[1]).addBands(zPair[1])
 
   return out;
 }
