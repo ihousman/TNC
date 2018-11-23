3489c551fa59c837cae9ec917e1619491e7f5086
ian.housman <ian.housman@gmail.com>
2018-11-23 13:53:36 +0000

    Updated gde-pulse/viewers/predictorViewer.js (added 2 lines, deleted 2 lines).

--- gde-pulse/viewers/predictorViewer.js
+++ gde-pulse/viewers/predictorViewer.js
@@ -134,7 +134,7 @@ function getPairDiff(c,year){
   var cT2  = ee.Image(c.filter(ee.Filter.calendarRange(year,year,'year')).first());
   
   var cSlpT  =cT2.subtract(cT1).float();
-  print('cslp',cT1,year)
+  // print('cslp',cT1,year)
   return [addPrefixToImageBandNames(cT2,'D0_'),addPrefixToImageBandNames(cSlpT,'D1_')];
 }
 
@@ -168,7 +168,7 @@ var out = ee.List.sequence(1992,1992).getInfo().map(function(yr){
    var fieldName ='Depth'+ yro.toString();
  
   
-  var igdesT = igdes.filter(ee.Filter.neq(fieldName, -999));
+  var igdesT = igdes.filter(ee.Filter.neq(fieldName, -999)).limit(10);
   var igdesTL = igdesT.toList(100000);
 
     // var rawPre = ee.Image(joinedRawForSlope.filter(ee.Filter.calendarRange(yr.subtract(1),yr.subtract(1),'year')).first());
