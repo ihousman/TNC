03f668fa84c44c65bb1c4109b04a54b6df1bc562
iwhousman <iwhousman@gmail.com>
2018-12-14 04:08:06 +0000

    Updated gde-pulse/r-analysis/predictorExtractor.js (added 2 lines, deleted 2 lines).

--- gde-pulse/r-analysis/predictorExtractor.js
+++ gde-pulse/r-analysis/predictorExtractor.js
@@ -1,6 +1,6 @@
 //Module imports
-var getImageLib = require('users/USFS_GTAC/modules:getImagesLib.js');
-var dLib = require('users/USFS_GTAC/modules:changeDetectionLib.js');
+var getImageLib = require('users/ianhousman/TNC:gde-pulse/modules/getImagesLib.js');
+var dLib = require('users/ianhousman/TNC:gde-pulse/modules/changeDetectionLib.js');
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 dLib.getExistingChangeData();
