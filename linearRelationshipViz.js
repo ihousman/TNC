ad6d783bae27ef7156f7d3376279a93247a50938
ian.housman <ian.housman@gmail.com>
2019-03-13 20:26:03 +0000

    Updated linearRelationshipViz.js (added 9 lines, deleted 5 lines).

--- linearRelationshipViz.js
+++ linearRelationshipViz.js
@@ -3,12 +3,13 @@ var dLib = require('users/ianhousman/TNC:gde-pulse/modules/changeDetectionLib.js
 
 var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018_v2');
 
-var years = ee.List.sequence(1985,2018).getInfo();
-var fields = years.map(function(yr){return 'Depth'+yr.toString()});
-print(fields);
+var years = ee.List.sequence(1985,2018);
+// var fields = years.map(function(yr){return 'Depth'+yr.toString()});
+// print(fields);
 // var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDEveg053_Landsat_dissolved_8022018_1000m_Depth_gwell_macro_veg_final_gt5yr_lt30ft');
 var igdesC = years.map(function(yr){
-  var field = 'Depth' + yr.toString();
+  yr = ee.Number(yr)
+  var field = 'Depth' + ee.String(yr);
   var img = igdes.reduceToImage([field],ee.Reducer.first());
   img = img.updateMask(img.neq(-999)).rename(['Depth_to_gw'])
   img = img.set('system:time_start',ee.Date.fromYMD(yr,6,1).millis());
@@ -20,4 +21,7 @@ c = c.map(function(img){return dLib.multBands(img,1,0.001)}).select(['.*NDMI']);
 igdesC = ee.ImageCollection(igdesC)
 
 var joined = getImageLib.joinCollections(c,igdesC)
+
+var fit = joined.reduce(ee.Reducer.linearRegression(1));
+print(fit)
 Map.addLayer(joined)
