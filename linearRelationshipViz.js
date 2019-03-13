043d44d3976ac1bac4f2d0689cac379a564e0aea
ian.housman <ian.housman@gmail.com>
2019-03-13 22:47:44 +0000

    Updated linearRelationshipViz.js (added 14 lines, deleted 20 lines).

--- linearRelationshipViz.js
+++ linearRelationshipViz.js
@@ -4,39 +4,33 @@ var dLib = require('users/ianhousman/TNC:gde-pulse/modules/changeDetectionLib.js
 var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018_v2');
 
 var years = ee.List.sequence(1985,2018).getInfo();
-// var fields = years.map(function(yr){return 'Depth'+yr.toString()});
-// print(fields);
-// var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDEveg053_Landsat_dissolved_8022018_1000m_Depth_gwell_macro_veg_final_gt5yr_lt30ft');
 var igdesC = years.map(function(yr){
-  yr = ee.Number(yr)
+  yr = ee.Number(yr);
   var field = ee.String('Depth').cat(ee.String(yr));
-  
   var img = igdes.reduceToImage([field],ee.Reducer.first());
-  img = img.updateMask(img.neq(-999)).rename(['Depth_to_gw']).multiply(-1)
+  img = img.updateMask(img.neq(-999)).rename(['Depth_to_gw']).multiply(-1);
   img = img.set('system:time_start',ee.Date.fromYMD(yr,6,1).millis());
-  return img
+  return img;
 });
-var c =ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection');
-c = c.map(function(img){return dLib.multBands(img,1,0.001)}).select(['.*NDMI']);
+var ltC =ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection');
+ltC = ltC.map(function(img){return dLib.multBands(img,1,0.001)}).select(['.*NDMI']);
 
-igdesC = ee.ImageCollection.fromImages(igdesC)
+igdesC = ee.ImageCollection.fromImages(igdesC);
 
-var joined = getImageLib.joinCollections(c,igdesC)
+var joined = getImageLib.joinCollections(ltC,igdesC);
 joined = joined.map(function(img){
   var c = ee.Image(1);
-  return c.addBands(img).copyProperties(img,['system:time_start'])
+  return c.addBands(img).copyProperties(img,['system:time_start']);
 });
-print(joined)
+
 var fit = joined.reduce(ee.Reducer.linearRegression(2,1));
 var model = fit.select(['coefficients']).arrayProject([0]).arrayFlatten([['intercept','slope']]);
 var predicted = joined.map(function(img){
   var pred = img.select([1]).multiply(model.select(['slope'])).add(model.select(['intercept'])).rename(['Depth_to_gw_pred']);
-  return img.addBands(pred).select([1,2,3])
+  return img.addBands(pred).select([1,2,3]);
 });
 Map.addLayer(predicted,{},'Fitted Time Series',false);
-// Map.addLayer(fit)
-var error = fit.select(['residuals']).arrayFlatten([['error']])
-// print(fit)
-// Map.addLayer(joined,{},'ts',false);
-// Map.addLayer(fit)
-Map.addLayer(error.abs(),{min:0,max:10,'palette':'00F,F00'},'Residual')
+
+var error = fit.select(['residuals']).arrayFlatten([['error']]);
+
+Map.addLayer(error.abs(),{min:0,max:10,'palette':'00F,F00'},'Residual');
