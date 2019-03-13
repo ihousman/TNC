dd4a300ef93c1792826897bd056d26fe2e7c2237
ian.housman <ian.housman@gmail.com>
2019-03-13 20:41:11 +0000

    Updated linearRelationshipViz.js (added 15 lines, deleted 9 lines).

--- linearRelationshipViz.js
+++ linearRelationshipViz.js
@@ -3,25 +3,31 @@ var dLib = require('users/ianhousman/TNC:gde-pulse/modules/changeDetectionLib.js
 
 var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018_v2');
 
-var years = ee.List.sequence(1985,2018);
+var years = ee.List.sequence(1985,2018).getInfo();
 // var fields = years.map(function(yr){return 'Depth'+yr.toString()});
 // print(fields);
 // var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDEveg053_Landsat_dissolved_8022018_1000m_Depth_gwell_macro_veg_final_gt5yr_lt30ft');
 var igdesC = years.map(function(yr){
   yr = ee.Number(yr)
-  var field = 'Depth' + ee.String(yr);
+  var field = ee.String('Depth').cat(ee.String(yr));
+  
   var img = igdes.reduceToImage([field],ee.Reducer.first());
-  img = img.updateMask(img.neq(-999)).rename(['Depth_to_gw'])
+  img = img.updateMask(img.neq(-999)).rename(['Depth_to_gw']).multiply(-1)
   img = img.set('system:time_start',ee.Date.fromYMD(yr,6,1).millis());
   return img
 });
 var c =ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection');
-c = c.map(function(img){return dLib.multBands(img,1,0.001)}).select(['.*NDMI']);
+c = c.map(function(img){return dLib.multBands(img,-1,0.001)}).select(['.*NDMI']);
 
-igdesC = ee.ImageCollection(igdesC)
+igdesC = ee.ImageCollection.fromImages(igdesC)
 
 var joined = getImageLib.joinCollections(c,igdesC)
-
-var fit = joined.reduce(ee.Reducer.linearRegression(1));
-print(fit)
-Map.addLayer(joined)
+joined = joined.map(function(img){
+  var c = ee.Image(1);
+  return c.addBands(img).copyProperties(img,['system:time_start'])
+});
+print(joined)
+var fit = joined.reduce(ee.Reducer.linearRegression(1,1));
+// print(fit)
+Map.addLayer(joined);
+Map.addLayer(fit)
