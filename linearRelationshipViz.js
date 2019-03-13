3748a5a878b3853f357ccef8b80714ee50eddd4b
ian.housman <ian.housman@gmail.com>
2019-03-13 20:22:28 +0000

    Updated linearRelationshipViz.js (added 12 lines, deleted 5 lines).

--- linearRelationshipViz.js
+++ linearRelationshipViz.js
@@ -1,3 +1,6 @@
+var getImageLib = require('users/ianhousman/TNC:gde-pulse/modules/getImagesLib.js');
+var dLib = require('users/ianhousman/TNC:gde-pulse/modules/changeDetectionLib.js');
+
 var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018_v2');
 
 var years = ee.List.sequence(1985,2018).getInfo();
@@ -7,10 +10,14 @@ print(fields);
 var igdesC = years.map(function(yr){
   var field = 'Depth' + yr.toString();
   var img = igdes.reduceToImage([field],ee.Reducer.first());
-  img = img.updateMask(img.neq(-999))
+  img = img.updateMask(img.neq(-999)).rename(['Depth_to_gw'])
   img = img.set('system:time_start',ee.Date.fromYMD(yr,6,1).millis());
   return img
 });
-var c 
+var c =ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection');
+c = c.map(function(img){return dLib.multBands(img,1,0.0001)}).select(['.*NDMI']);
+
 igdesC = ee.ImageCollection(igdesC)
-Map.addLayer(igdesC)
+
+var joined = getImageLib.joinCollections(c,igdesC)
+Map.addLayer(joined)
