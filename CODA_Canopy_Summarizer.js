9cbbad513bd558d4beacab400085731524cf8f4e
ian.housman <ian.housman@gmail.com>
2019-08-26 18:18:07 +0000

    Updated CODA_Canopy_Summarizer.js (added 3 lines, deleted 1 line).

--- CODA_Canopy_Summarizer.js
+++ CODA_Canopy_Summarizer.js
@@ -30,7 +30,9 @@ var blocks = ee.FeatureCollection('TIGER/2010/Blocks').filterBounds(msas);
 var canopy = ee.ImageCollection(canopyCollection).filterBounds(msas).mosaic().unmask();
 canopy = getImagesLib.setNoData(canopy.clip(msas),2);
 
-var temperature = getImagesLib.getProcessedLandsatScenes(msas,startYear,endYear,startJulian,endJulian).select(['temp']).median().clip(msas);
+var temperature = getImagesLib.getProcessedLandsatScenes(msas,startYear,endYear,startJulian,endJulian);
+print(temperature.first())
+temperature = temperature.select(['temp']).median().clip(msas);
 ///////////////////////////////////////////////////////////////////////////////
 Map.addLayer(canopy,{min:0,max:2,palette:'000,0F0,F00'},'Canopy',false);
 Map.addLayer(temperature,{min:280,max:320,palette:'00F,888,F00'},'Temperature',false);
