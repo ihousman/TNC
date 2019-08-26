0214adc98b97e1d6117e723c3ebb0818023d5750
ian.housman <ian.housman@gmail.com>
2019-08-26 17:58:12 +0000

    Updated CODA_Canopy_Summarizer.js (added 1 line, deleted 1 line).

--- CODA_Canopy_Summarizer.js
+++ CODA_Canopy_Summarizer.js
@@ -74,4 +74,4 @@ summaries = summaries.map(function(f){return f.select(propsOld, propsNew)});
 
 print(summaries)
 
-Export.table.toAsset(summaries, 'blocks-canopy-cover-stats2', 'users/ianhousman/urban-canopy/blocks-canopy-cover-stats2')
+Export.table.toAsset(summaries, 'blocks-canopy-cover-stats', 'users/ianhousman/urban-canopy/blocks-canopy-cover-stats')
