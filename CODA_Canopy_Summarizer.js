17028e35600d26bce43a3e933135f9b05eb23c06
ian.housman <ian.housman@gmail.com>
2019-08-26 17:16:15 +0000

    Updated CODA_Canopy_Summarizer.js (added 4 lines, deleted 6 lines).

--- CODA_Canopy_Summarizer.js
+++ CODA_Canopy_Summarizer.js
@@ -19,27 +19,25 @@ var canopyReducer = ee.Reducer.fixedHistogram(0, 3, 3);
 ///////////////////////////////////////////////////////////////////////////////
 //Load asset with City Boundaries with 102 records, but we are mapping forest for only for 100 dropped 2 cities in PR
 //////////////////////////////////////////////////////////////////////////////
-
+//Get data
 var msas =ee.FeatureCollection(msaOutlines).filter(ee.Filter.inList('zone',zoneList));
 
-
 var blocks = ee.FeatureCollection('TIGER/2010/Blocks').filterBounds(msas);
-
 
 var canopy = ee.ImageCollection(canopyCollection).filterBounds(msas).mosaic().unmask();
 canopy = getImagesLib.setNoData(canopy.clip(msas),2);
 
 var temperature = getImagesLib.getProcessedLandsatScenes(msas,startYear,endYear,startJulian,endJulian).select(['temp']).median().clip(msas);
-
+///////////////////////////////////////////////////////////////////////////////
 Map.addLayer(canopy,{min:0,max:2,palette:'000,0F0,F00'},'Canopy',false);
 Map.addLayer(temperature,{min:280,max:320,palette:'00F,888,F00'},'Temperature',false);
 Map.addLayer(blocks,{},'Blocks',false);
 Map.addLayer(msas,{},'MSAs',false);
-
+///////////////////////////////////////////////////////////////////////////////
 blocks = blocks.limit(10);
 var summaries =temperature.reduceRegions(blocks, tempReducer, 30, 'EPSG:5070', null, 1) ;
 
-
+print(summaries)
 // function summarizeAreas(areas,image,scale,propertyNameOut,reducer){
 //   var props = ee.Feature(areas.first()).propertyNames();
 //   print(props);
