6e6ee04a39c0b6ba57c816ce1e3e5fd7cee6f933
ian.housman <ian.housman@gmail.com>
2019-09-10 03:24:26 +0000

    Updated CODA_Canopy_Summarizer.js (added 2 lines, deleted 2 lines).

--- CODA_Canopy_Summarizer.js
+++ CODA_Canopy_Summarizer.js
@@ -125,8 +125,8 @@ function summarize(f){
   var tempHistK = ee.List(tempHist.keys());
   var tempHistOut= ee.List(tempHist.values()).map(function(k){
     k = ee.List(k);
-    var tk = ee.Array(k).slice(1,0,1).project([1]).toList();
-    var tv = ee.Array(k).slice(1,1,2).project([1]).toList();
+    var tk = ee.Array(k).slice(1,0,1).project([0]).toList();
+    var tv = ee.Array(k).slice(1,1,2).project([0]).toList();
     return ee.Dictionary.fromLists(tk,tv)
     
   });
