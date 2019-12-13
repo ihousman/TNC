81d3983bc50c966815b503cfb47373761608e6c3
ian.housman <ian.housman@gmail.com>
2019-12-13 03:20:42 +0000

    Updated gde-pulse/r-analysis/predictorExtractor.js (added 55 lines, deleted 48 lines).

--- gde-pulse/r-analysis/predictorExtractor.js
+++ gde-pulse/r-analysis/predictorExtractor.js
@@ -45,28 +45,29 @@ var indexEndWildcards = indexNames.map(function(bn){return '.*'+bn});
 var indexStartWildcards = indexNames.map(function(bn){return bn +'.*'});
 
 var analysisYears = ee.List.sequence(1985,2018);
-var exportYears = ee.List.sequence(1992,1992);
+var exportYears = ee.List.sequence(1985,2018);
 
 //Which igde polygons
 // var igdes = ee.FeatureCollection('projects/igde-work/igde-data/GDEpulse2018_iGDE_V1_20180802_joined_annual_depth_macro_veg');
-var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018_v2');
+// var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018_v2');
+var igdes = ee.FeatureCollection('projects/igde-work/igde-data/iGDE_AnnualDepth_renamed_oct2018');
 print(igdes.size());
 // var igdeCount = 15419;//igdes.size().getInfo();
 // var igdesL = igdes.toList(10000000,0);
 
 //Define how many igdes to extract at once- reduce to increase stability
-var howMany = 1000;
+var howMany = 200000;
 
 //////////////////////////////////////////////////////////////////////////////////////
 //Bring in composites
-var composites = ee.ImageCollection('projects/igde-work/raster-data/composite-collection')
-        .sort('system:time_start')
-        .map(function(img){return dLib.multBands(img,1,0.0001)})
-        .map(getImageLib.simpleAddIndices)
-        .map(getImageLib.getTasseledCap)
-        .map(getImageLib.simpleAddTCAngles)
-        .map(getImageLib.addSAVIandEVI)
-        .select(indexNamesComposites);
+// var composites = ee.ImageCollection('projects/igde-work/raster-data/composite-collection')
+//         .sort('system:time_start')
+//         .map(function(img){return dLib.multBands(img,1,0.0001)})
+//         .map(getImageLib.simpleAddIndices)
+//         .map(getImageLib.getTasseledCap)
+//         .map(getImageLib.simpleAddTCAngles)
+//         .map(getImageLib.addSAVIandEVI)
+//         .select(indexNamesComposites);
         
 //Bring in Landtrendr outputs
 var lt = ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collection')
@@ -74,56 +75,56 @@ var lt = ee.ImageCollection('projects/igde-work/raster-data/LANDTRENDR-collectio
         .map(function(img){return dLib.multBands(img,1,0.0001)});
 
 //Bring in harmonics
-var harmonics = ee.ImageCollection('projects/igde-work/raster-data/harmonic-coefficients-collection');
+// var harmonics = ee.ImageCollection('projects/igde-work/raster-data/harmonic-coefficients-collection');
 
 //Bring in z and trend
-var zTrend =ee.ImageCollection('projects/igde-work/raster-data/z-score-trend-collection');
+// var zTrend =ee.ImageCollection('projects/igde-work/raster-data/z-score-trend-collection');
 
 //Bring in DAYMET
-var daymet = ee.ImageCollection('projects/igde-work/raster-data/DAYMET-Collection');
+// var daymet = ee.ImageCollection('projects/igde-work/raster-data/DAYMET-Collection');
 
 //Add prefix to DAYMET
-daymet = addPrefixToCollectionBandNames(daymet,'dymt_');
+// daymet = addPrefixToCollectionBandNames(daymet,'dymt_');
 
 //Separate z and trend and rescale them to their raw value
-var z = zTrend.select(['.*_Z'])
-  .map(function(img){return dLib.multBands(img,1,0.1)});
-var trend = zTrend.select(['.*._slope'])
-  .map(function(img){return dLib.multBands(img,1,0.0001)});
+// var z = zTrend.select(['.*_Z'])
+//   .map(function(img){return dLib.multBands(img,1,0.1)});
+// var trend = zTrend.select(['.*._slope'])
+//   .map(function(img){return dLib.multBands(img,1,0.0001)});
 
 //Join them back together
-zTrend = getImageLib.joinCollections(z,trend,false);
+// zTrend = getImageLib.joinCollections(z,trend,false);
 
 //Seperate two date periods z-score was processed for and set to same date
-var zTrend1 = zTrend.filter(ee.Filter.calendarRange(121,185))
-        .map(function(img){
-          var y = ee.Date(img.get('system:time_start')).get('year');
-          return img.set('system:time_start',ee.Date.fromYMD(y,6,1).millis());
-        });
-var zTrend2 = zTrend.filter(ee.Filter.calendarRange(185,249))
-        .map(function(img){
-          var y = ee.Date(img.get('system:time_start')).get('year');
-          return img.set('system:time_start',ee.Date.fromYMD(y,6,1).millis());
-        });
+// var zTrend1 = zTrend.filter(ee.Filter.calendarRange(121,185))
+//         .map(function(img){
+//           var y = ee.Date(img.get('system:time_start')).get('year');
+//           return img.set('system:time_start',ee.Date.fromYMD(y,6,1).millis());
+//         });
+// var zTrend2 = zTrend.filter(ee.Filter.calendarRange(185,249))
+//         .map(function(img){
+//           var y = ee.Date(img.get('system:time_start')).get('year');
+//           return img.set('system:time_start',ee.Date.fromYMD(y,6,1).millis());
+//         });
 
 //Add ending to band names to denote which period
-zTrend1 = addSuffixToCollectionBandNames(zTrend1,'_121_185');
-zTrend2 = addSuffixToCollectionBandNames(zTrend2,'_185_249');
+// zTrend1 = addSuffixToCollectionBandNames(zTrend1,'_121_185');
+// zTrend2 = addSuffixToCollectionBandNames(zTrend2,'_185_249');
 
 //Join them back together
-zTrend = getImageLib.joinCollections(zTrend1,zTrend2,false);
+// zTrend = getImageLib.joinCollections(zTrend1,zTrend2,false);
 
 //Get phase, amplitude, peak and AUC of the harmonic functions
-var pap = harmonics
-    .map(function(img){return dLib.multBands(img,1,0.001)})
-    .map(getImageLib.getPhaseAmplitudePeak)
-    .select(['.*_phase','.*_amplitude','.*peakJulianDay','.*AUC']);
+// var pap = harmonics
+//     .map(function(img){return dLib.multBands(img,1,0.001)})
+//     .map(getImageLib.getPhaseAmplitudePeak)
+//     .select(['.*_phase','.*_amplitude','.*peakJulianDay','.*AUC']);
 
 //Pull individual components out
-var amplitudes = pap.select(['.*_amplitude']);
-var phases = pap.select(['.*_phase']);
-var peakJulians = pap.select(['.*peakJulianDay']);
-Map.addLayer(peakJulians);
+// var amplitudes = pap.select(['.*_amplitude']);
+// var phases = pap.select(['.*_phase']);
+// var peakJulians = pap.select(['.*peakJulianDay']);
+// Map.addLayer(peakJulians);
     
 
 
@@ -157,13 +158,19 @@ function getPairDiff(c,year){
 //Wrapper to get all the data for a given year
 function getYr(year){
   var igdeyrPair = getPairDiff(igdeyr,year);
-  var compPair = getPairDiff(composites,year);
+  // var compPair = getPairDiff(composites,year);
   var ltPair = getPairDiff(lt,year);
-  var papPair = getPairDiff(pap,year);
-  var daymetPair = getPairDiff(daymet,year);
-  var zPair = getPairDiff(zTrend,year);
-  var out =ee.Image(igdeyrPair[0]).addBands(compPair[0]).addBands(ltPair[0]).addBands(papPair[0]).addBands(daymetPair[0])
-            .addBands(igdeyrPair[1]).addBands(compPair[1]).addBands(ltPair[1]).addBands(papPair[1]).addBands(daymetPair[1]).addBands(zPair[1]);
+  // var papPair = getPairDiff(pap,year);
+  // var daymetPair = getPairDiff(daymet,year);
+  // var zPair = getPairDiff(zTrend,year);
+  var out =ee.Image(igdeyrPair[0])
+  // .addBands(compPair[0])
+  .addBands(ltPair[0])
+  // .addBands(papPair[0]).addBands(daymetPair[0])
+            // .addBands(igdeyrPair[1])
+            .addBands(compPair[1])
+            .addBands(ltPair[1])
+            // .addBands(papPair[1]).addBands(daymetPair[1]).addBands(zPair[1]);
 
   return out;
 }
@@ -197,7 +204,7 @@ var out = exportYears.getInfo().map(function(yr){
     var outName = 'Export-Full-Dataset-'+yro.toString() + '_'+startI.toString() + '_' + (endI-1).toString();
     // print(outName)
     if(failedExports.indexOf(outName)>-1){
-      var outTable = forExtraction.reduceRegions(ee.FeatureCollection(igdesTLT), ee.Reducer.mean(), scale, crs, transform, 2);
+      var outTable = forExtraction.reduceRegions(ee.FeatureCollection(igdesTLT), ee.Reducer.first(), scale, crs, transform, 2);
     outTable = outTable.map(function(f){return f.set('A_Year',yr)});
     Export.table.toDrive(outTable, outName, 'TNC-GDEPulse-GEE-Export-Tables');
     
